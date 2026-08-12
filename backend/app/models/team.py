e5f334c30ebcd22ccad266ba864726ab1a5829f8
Tyler Forgione <tyler.forgione26@gmail.com>
2026-08-12 00:37:27 +0000

    Create team model

---
+++ backend/app/models/team.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer, String, Column, Boolean
+from app.db.base import Base
+
+
+class Team(Base):
+    __tablename__ = "teams"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    abbrev = Column(String(5), nullable=False)
+    logo = Column(String(255))
+    dark_logo = Column(String(255))
+    first_season = Column(String(8))
+    last_season = Column(String(8))
+    is_active = Column(Boolean, default=False)
+    conference = Column(String(50))
+    division = Column(String(50))
